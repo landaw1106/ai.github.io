b0357f31b2ecbbf97d12238d0895d83934f6dee8
landaw1106 <a20011106a@outlook.com>
2019-09-13 22:39:59 -0700

    delete firebase

--- docs/script.js
+++ docs/script.js
@@ -13,10 +13,3 @@ function checkForm(form)
   // validation was successful
   return true;
 }
-
-function processData(form) {
-  var data = form.feedback.value;
-  firebase.database().ref('feedbacks/' + data).set({
-    feedback: data
-  });
-}
